6aeb143c1cffa49057af6c6295550a9688fbb629
Aswin Thulasi R <125785982+Aswinrthulasi@users.noreply.github.com>
2024-09-09 12:13:17 +0530

    all commit including node modules

---
+++ fd.js
@@ -0,0 +1,5 @@
+function helloMessage(inp){
+    console.log(`Hi ${inp}, its nice to see you Today`);
+}
+
+module.exports.display=helloMessage
---
+++ node.js
@@ -0,0 +1,9 @@
+var filename=require('./fd');
+var colors=require('colors');
+var osdetails=require('os');
+
+filename.display('Aswin');
+
+console.log('hello'.rainbow);
+
+console.log(osdetails.hostname());
